f7a862395469f2aa30d41eb05f8c1919e214fb4b
Vladislav Voitovich <vladislav.voitovich@team.wrike.com>
2021-05-26 12:41:42 +0300

    Arrays

--- js/arrays.js
+++ js/arrays.js
@@ -13,9 +13,9 @@ arr.push(10); // добавляет новый эелемнт в конце ма
 // }
 
 // Перебор значений массива
-for (let value of arr) {
-    console.log(value);
-}
+// for (let value of arr) {
+//     console.log(value);
+// }
 
 // arr[99] = 0;
 // console.log(arr.length); // -> последний индекс + 1
@@ -25,6 +25,24 @@ for (let value of arr) {
 // МЕТОД = ФУНКЦИЯ
 
 // Мы перебираем массив и можем к каждому элементу массива применить что-то (функцию). 
-arr.forEach(function(item, i, arr){
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+// arr.forEach(function(item, i, arr){
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+const str = prompt("", "");
+const products = str.split(", "); // разделили строчку и получили массив элементов. Чтобы метод сработал, в строке элементы должны разделяться через запятую + пробел
+// console.log(products); 
+products.sort(); // отсоритировалось в алфавитном порядке, он всегда сортирует только массивы со строчными элементами
+console.log(products.join('; ')); // сделали из строки массив с разделителем ; + пробел
+
+const arrNums = [2, 13, 25, 8, 10];
+arrNums.sort(); // [ 10, 13, 2, 25, 8 ]
+arrNums.sort(compareNum); // [ 2, 8, 10, 13, 25 ]
+console.log(arrNums);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+// Псевдомассивы
+// У псевдомассивов нет методов - они нужны только для хранения данных
