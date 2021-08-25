7ff68c22ba67381520e195e8abb3ff271e05a9c3
Vladislav Voitovich <vladislav.voitovich@team.wrike.com>
2021-08-25 11:59:56 +0300

    Ajax form message

--- 08_project_food/js/script.js
+++ 08_project_food/js/script.js
@@ -100,8 +100,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // Start: Modal --------------------------
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalCloseBtn = document.querySelector('[data-close-modal]');
+          modal = document.querySelector('.modal');
 
     function openModal() {
         modal.classList.add('show');
@@ -120,10 +119,8 @@ window.addEventListener("DOMContentLoaded", () => {
         btn.addEventListener('click', openModal);
     });
 
-    modalCloseBtn.addEventListener('click', closeModal);
-
     modal.addEventListener('click', (e) => {
-        if(e.target === modal) {
+        if(e.target === modal || e.target.getAttribute('data-close') == "") {
             closeModal();
         }
     });
@@ -137,7 +134,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Start: Scroll event --------------------------
 
     // Timer - показывать modal после 5сек, но если пользователь уже открывал модальное окно, то его не нужно показывать снова
-    // const modalTimerId = setTimeout(openModal, 30000);
+    const modalTimerId = setTimeout(openModal, 50000);
 
     // Scroll
     function showModalByScroll() {
@@ -235,7 +232,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const forms = document.querySelectorAll('form');
 
     const message = {
-        loading: 'Загрузка',
+        loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжимся',
         failure: 'Что-то пошло не так...'
     };
@@ -248,10 +245,13 @@ window.addEventListener("DOMContentLoaded", () => {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('div');
-            statusMessage.classList.add('status');
-            statusMessage.textContent = message.loading;
-            form.append(statusMessage);
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
 
             const request = new XMLHttpRequest();
             request.open('POST', 'server.php');
@@ -271,16 +271,38 @@ window.addEventListener("DOMContentLoaded", () => {
             request.addEventListener('load', () => {
                 if (request.status === 200) {
                     console.log(request.response);
-                    statusMessage.textContent = message.success;
+                    showThanksModal(message.success);
                     form.reset();
-                    setTimeout(() => {
-                        statusMessage.remove();
-                    }, 2000);
+                    statusMessage.remove();
                 } else {
-                    statusMessage.textContent = message.failure;
+                    showThanksModal(message.failure);
                 }
             });
         });
+    }
+
+    function showThanksModal(message) {
+        const prevModalDialog = document.querySelector('.modal__dialog');
+
+        prevModalDialog.classList.add('hide');
+        openModal();
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+              <div class="modal__close" data-close>&times;</div>
+              <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout(() => {
+            thanksModal.remove();
+            prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 4000);
     }
 
     // End: AJAX FORM --------------------------
