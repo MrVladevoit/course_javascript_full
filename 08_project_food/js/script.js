89fb03d991fc04ac1b0063635c7807c8251765dd
Vladislav Voitovich <vladislav.voitovich@team.wrike.com>
2021-08-31 14:09:19 +0300

    Remove menuCard Class

--- 08_project_food/js/script.js
+++ 08_project_food/js/script.js
@@ -150,46 +150,6 @@ window.addEventListener("DOMContentLoaded", () => {
     // End: Modal --------------------------
 
     // Start: Classes for cards --------------------------
-    class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.price = price;
-            this.descr = descr;
-            this.classes = classes;
-            this.parent = document.querySelector(parentSelector);
-            this.transfer = 27;
-            this.changeToUAH();
-        }
-
-        changeToUAH() {
-            this.price = this.price * this.transfer;
-        }
-
-        render() {
-            const element = document.createElement('div');
-            
-            if(this.classes.length === 0) {
-                this.element = 'menu__item';
-                element.classList.add(this.element);
-            } else {
-                this.classes.forEach(className => element.classList.add(className));
-            }
-
-            element.innerHTML = `
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
-            `;
-            this.parent.append(element);
-        }
-    }
 
     const getResource = async (url) => {
         const result = await fetch(url);
@@ -201,12 +161,31 @@ window.addEventListener("DOMContentLoaded", () => {
         return await result.json();
     };
 
+
     getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
-               new MenuCard(img, altimg,title, descr, price, '.menu .container', 'menu__item').render(); 
-            });
+        .then(data => createCard(data));
+    
+    function createCard(data) {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            const element = document.createElement('div');
+            element.classList.add('menu__item');
+
+            price = price * 27;
+
+            element.innerHTML = `
+                <img src=${img} alt=${altimg}>
+                <h3 class="menu__item-subtitle">${title}</h3>
+                <div class="menu__item-descr">${descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                </div>
+            `;
+            
+            document.querySelector('.menu .container').append(element);
         });
+    }
 
     // End: Classes for cards --------------------------
     
